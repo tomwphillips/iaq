b644ed143b56cdd0c2bbbd8d4c2e14afd3df9f26
Tom Phillips <me@tomwphillips.co.uk>
2021-12-31 13:51:28 +0000

    Delete setup.py
    Use pipenv to deploy instead.

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="pms5003-logger",
-    description="Raspberry Pi-based Logger for PMS5003 air pollution sensor",
-    url="https://github.com/tomwphillips/pms5003-logger",
-    py_modules=["pms5003logger"],
-    install_requires=["pms5003==0.0.5"],
-)
