7667122385f164dfe25242585295c19b94e7644c
Tom Phillips <me@tomwphillips.co.uk>
2021-12-31 14:19:08 +0000

    Switch from pipenv to pip
    Pain installing pipenv (and pyenv) on Raspbian.
    This is simpler.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pms5003-logger",
+    description="Raspberry Pi logger for PMS5003 air pollution sensor",
+    url="https://github.com/tomwphillips/pms5003-logger",
+    py_modules=["pms5003logger"],
+    install_requires=["pms5003==0.0.5"],
+    extras_require={"dev": ["black", "pytest", "isort", "flake8"]},
+)
