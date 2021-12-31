6d3d4dee84b73e91945a3ff2373cdd6d590e253d
Tom Phillips <me@tomwphillips.co.uk>
2021-12-31 13:45:56 +0000

    Drop version and python version from setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='pms5003-logger',
-    version='0.1.0', 
-    description='Raspberry Pi-based Logger for PMS5003 air pollution sensor',
-    url='https://github.com/tomwphillips/pms5003-logger',
+    name="pms5003-logger",
+    description="Raspberry Pi-based Logger for PMS5003 air pollution sensor",
+    url="https://github.com/tomwphillips/pms5003-logger",
     py_modules=["pms5003logger"],
-    python_requires='>=3.6, <4',
-    install_requires=['pms5003==0.0.5']
+    install_requires=["pms5003==0.0.5"],
 )
