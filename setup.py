c74c8df5e0ac3cb7ecbc05ca7ead7c8853a8521e
Tom Phillips <me@tomwphillips.co.uk>
2022-01-02 13:27:14 +0000

    Delete viewer
    Despite using a wheel for numpy from piwheels.org, importing numpy fails
    with an illegal instruction error.
    
    Painfully slow on an RPi 1. Not worth any more effort.

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name="pms5003-logger",
     description="Raspberry Pi logger for PMS5003 air pollution sensor",
     url="https://github.com/tomwphillips/pms5003-logger",
-    py_modules=["pms5003logger", "pms5003viewer"],
-    install_requires=["pms5003==0.0.5", "matplotlib", "flask"],
+    py_modules=["pms5003logger"],
+    install_requires=["pms5003==0.0.5"],
     extras_require={
         "dev": ["black", "pytest", "isort", "flake8"],
     },
